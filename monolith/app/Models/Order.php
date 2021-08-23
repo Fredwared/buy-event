218dd3fa77e474f1a94b0e009c2325ec28d0be4b
Golib Khalimov <Fredwared@gmail.com>
2021-08-23 14:50:38 +0500

    Property reference

--- monolith/app/Models/Order.php
+++ monolith/app/Models/Order.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
+/**
+ * @property HasOne customer
+ */
 class Order extends Model
 {
     use HasFactory;
